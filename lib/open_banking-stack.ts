88724016fc1a3d93c997114f3fea4de2c9d58b04
GH Portefait <gh@portefait-golub.com>
2019-12-18 23:00:17 +0100

    minor tweaks

--- lib/open_banking-stack.ts
+++ lib/open_banking-stack.ts
@@ -1,11 +1,11 @@
-import cdk = require( '@aws-cdk/core' );
-import { CommandStack } from './command_stack';
+import { CommandStack } from './command_stack'
+import cdk = require('@aws-cdk/core');
 
 export class OpenBankingStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor (scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
 
     // The code that defines your stack goes here
-    new CommandStack( this, 'Command Stack' );
+    new CommandStack(this, 'Command Stack')
   }
 }
